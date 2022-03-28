1e7ac9350e0ccc62100a817cbe98bcb5d82222b8
asj <asj9674@gmail.com>
2022-03-28 11:12:00 +0900

    bind

--- javascript/js-object/object_function.js
+++ javascript/js-object/object_function.js
@@ -14,6 +14,8 @@ function sum(prefix){
     return (this.first + this.second) + prefix;
 }
 
-console.log('sum.call(ahn)',sum.call(ahn, '이건'));
-console.log('sum.call(kim)',sum.call(kim, '뭘까'));
+console.log('sum.call(ahn)',sum.call(ahn, ' -> 이건 ahn객체를 인자로 갖는 call 메소드'));
+console.log('sum.call(kim)',sum.call(kim, ' -> 이건 kim객체를 인자로 갖는 call 메소드'));
 
+let kimSum = sum.bind(kim, ' => 이건 kim객체를 고정된 인자 값으로 갖는 bind 메소드');
+console.log('kimSum()', kimSum());
